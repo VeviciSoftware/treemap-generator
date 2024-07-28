7fcefc3d915a8d75d5ed6244406860227ebed95a
Felipe Alexandre Duarte Araújo Ramos <felipeadar108@gmail.com>
2024-07-28 19:47:04 -0300

    :lipstick: style: alterando de typography para listItem

--- src/assets/components/HowToUse.jsx
+++ src/assets/components/HowToUse.jsx
@@ -1,4 +1,11 @@
-import { Paper, Typography, Box, List, ListItem, ListItemText } from '@mui/material';
+import {
+  Paper,
+  Typography,
+  Box,
+  List,
+  ListItem,
+  ListItemText,
+} from "@mui/material";
 
 const HowToUse = () => {
   return (
@@ -18,20 +25,31 @@ const HowToUse = () => {
         <Typography variant="subtitle1" component="div">
           JSON:
         </Typography>
-        <Box component="img" src="/public/JSONExample.png" alt="Exemplo de arquivo JSON" sx={{ width: '100%', mt: 1, mb: 2 }} />
+        <Box
+          component="img"
+          src="/public/JSONExample.png"
+          alt="Exemplo de arquivo JSON"
+          sx={{ width: "100%", mt: 1, mb: 2 }}
+        />
       </Box>
       <Box sx={{ mb: 2 }}>
         <Typography variant="subtitle1" component="div">
           XLSX:
         </Typography>
-        <Box component="img" src="/public/XLSXExample.png" alt="Exemplo de arquivo XLSX" sx={{ width: '100%', mt: 1, mb: 2 }} />
+        <Box
+          component="img"
+          src="/public/XLSXExample.png"
+          alt="Exemplo de arquivo XLSX"
+          sx={{ width: "100%", mt: 1, mb: 2 }}
+        />
       </Box>
       <Typography variant="body1" component="div" sx={{ mb: 2 }}>
-        A 2° coluna determina a área do item no TreeMap e a 3° coluna determina a cor do item.
+        A 2° coluna determina a área do item no TreeMap e a 3° coluna determina
+        a cor do item.
       </Typography>
-      <Typography variant="body1" component="div">
-        3. Com as informações carregadas, o TreeMap será gerado automaticamente.
-      </Typography>
+      <ListItem>
+        <ListItemText primary="3. Com as informações carregadas, o TreeMap será gerado automaticamente." />
+      </ListItem>
     </Paper>
   );
 };
